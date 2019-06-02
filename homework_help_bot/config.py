33e42b9e71889e52733adc199dd9af20991fd435
leeweiminsg <40821628+leeweiminsg@users.noreply.github.com>
2019-06-02 21:35:11 +0800

    update to production settings

--- homework_help_bot/config.py
+++ homework_help_bot/config.py
@@ -1,9 +1,9 @@
 # Dev token: 824843960:AAGY9fLHg4yMPnQbSuvFDu1qiefPbX8hAHY
 # Production token: 837770119:AAEmP97SuF0moJezC0jipSHz5Uy-CXqNNoI
-TOKEN = '824843960:AAGY9fLHg4yMPnQbSuvFDu1qiefPbX8hAHY'
+TOKEN = '837770119:AAEmP97SuF0moJezC0jipSHz5Uy-CXqNNoI'
 # Dev db url: 'mongodb://localhost:27017/' - default setting
 # Production db url: mongodb+srv://admin:14&Chs8FecVro!sVrD12iP0$k@homework-help-cluster-dlv15.mongodb.net/test?retryWrites=true
-DB_URL = 'mongodb://localhost:27017/'
+DB_URL = 'mongodb+srv://admin:14&Chs8FecVro!sVrD12iP0$k@homework-help-cluster-dlv15.mongodb.net/test?retryWrites=true'
 
 
 TUTOR_ID = 692107365
